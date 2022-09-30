2b0e6c35331c3c468bb0d187ef4a39203bf4f9e3
Natan <natan.gluszko@natwest.com>
2022-09-30 14:10:27 +0100

    add number to call a worse version of the phone app ;////

--- MakingCalls/ViewController.swift
+++ MakingCalls/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func btnCall(_ sender: Any) {
+        let url = URL(string: "tel://\(numberTextField.text!)")
+        
+        if UIApplication.shared.canOpenURL(url!) {
+            UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+        }
+    }
 }
 
