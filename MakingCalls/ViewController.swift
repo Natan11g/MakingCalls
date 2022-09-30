1f78763822b99eed12f290e56ceba90801e8563b
Natan <natan.gluszko@natwest.com>
2022-09-30 13:09:56 +0100

    Initial Commit

---
+++ MakingCalls/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MakingCalls
+//
+//  Created by Natan Gluszko on 30/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
